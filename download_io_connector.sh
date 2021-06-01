28d0dc22d83781b8306cc53beec927566783b539
ghostletters <ghost.letters@mailbox.org>
2021-06-01 20:49:12 +0200

    session refactor code into nicer state

--- download_io_connector.sh
+++ download_io_connector.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+set -xe
 
-mkdir -p docker/io_connectors
+mkdir -p docker/pulsar/io_connectors
 
-PULSAR_VERSION=$(mvn help:evaluate -Dexpression=pulsar.client.version -q -DforceStdout)
+PULSAR_VERSION=$(mvn help:evaluate -Dexpression=pulsar.version -q -DforceStdout)
 echo set pulsar version: ${PULSAR_VERSION} for docker-compose.yml
 echo
 
 sed -i "s/PULSAR_VERSION=.*/PULSAR_VERSION=${PULSAR_VERSION}/" ./docker/.env
 sed -i "s/PULSAR_VERSION.*/PULSAR_VERSION \`${PULSAR_VERSION}\`/" ./README.md
+sed -i "s/pulsar-io-debezium-postgres-.*/pulsar-io-debezium-postgres-${PULSAR_VERSION}.nar\"/" ./docker/pulsar/debezium-postgres-source-config.yaml
 
-curl https://downloads.apache.org/pulsar/pulsar-${PULSAR_VERSION}/connectors/pulsar-io-debezium-postgres-${PULSAR_VERSION}.nar \
-  --output docker/io_connectors/pulsar-io-debezium-postgres-${PULSAR_VERSION}.nar
+#curl https://downloads.apache.org/pulsar/pulsar-${PULSAR_VERSION}/connectors/pulsar-io-debezium-postgres-${PULSAR_VERSION}.nar \
+#  --output docker/io_connectors/pulsar-io-debezium-postgres-${PULSAR_VERSION}.nar
